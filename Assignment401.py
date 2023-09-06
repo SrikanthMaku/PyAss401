080b5adf83b94e14ab287a4e01214b086492105c
SrikanthMaku <136585124+SrikanthMaku@users.noreply.github.com>
2023-09-06 22:07:06 +0530

    Add files via upload

---
+++ Assignment401.py
@@ -0,0 +1,14 @@
+# Write a Python program to create a lambda function that adds 25 to a given number passed in as an argument.
+# sample input: 10
+# sample output: 35
+
+
+
+number = 10
+
+add_func = lambda num : num +25
+print((add_func(number)))
+
+
+
+
